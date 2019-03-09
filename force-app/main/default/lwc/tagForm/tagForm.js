16f798a8cba2ac3a9e4d6082a90cac6666311fcb
Eduardo Asenjo Rodriguez <asenjo.rodriguez.eduardo@gmail.com>
2019-03-09 01:03:23 +0000

    OnSuccess navigates to new record

--- force-app/main/default/lwc/tagForm/tagForm.js
+++ force-app/main/default/lwc/tagForm/tagForm.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class TagForm extends LightningElement {}
+export default class TagForm extends NavigationMixin(LightningElement) {
+    handleSuccess(event) {
+        const payload = event.detail;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: payload.id,
+                objectApiName: payload.apiName,
+                actionName: 'view'
+            }
+        });
+    }
+}
