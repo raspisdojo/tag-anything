22a94aeb2b03a5e57ad10c8c069c14a72f791d79
Eduardo Asenjo Rodriguez <asenjo.rodriguez.eduardo@gmail.com>
2019-04-05 22:48:42 +0100

    Run only repo test classes

--- assets/runTestAndDeploys.sh
+++ assets/runTestAndDeploys.sh
@@ -11,16 +11,17 @@ echo "Connecting to Environment"
 sfdx force:auth:jwt:grant --clientid $ENVIRONMENT --jwtkeyfile assets/server.key --username $USER --setdefaultdevhubusername -a DevHub
 mkdir temp_metadata
 sfdx force:source:convert -d temp_metadata/ -n Travis_CI_Package
+TEST_CLASSES=$(cd force-app/main/default/classes/ && ls -m *Test*.cls | tr -d ' ');
 if [ "$OPERATION" = "TestOnly" ]; then
     echo "Running Validation against" $ENVIRONMENT
-    sfdx force:mdapi:deploy -c -l RunLocalTests -d temp_metadata/ -u DevHub -w 10
+    sfdx force:mdapi:deploy -c -l RunSpecifiedTests -r $TEST_CLASSES -d temp_metadata/ -u DevHub -w 10
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 fi
 if [ "$OPERATION" = "Deploy" ]; then
     echo "Running Validation against" $ENVIRONMENT
-    sfdx force:mdapi:deploy -c -l RunLocalTests -d temp_metadata/ -u DevHub -w 10
+    sfdx force:mdapi:deploy -c -l RunSpecifiedTests -r $TEST_CLASSES -d temp_metadata/ -u DevHub -w 10
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
     echo "Running Deployment against" $ENVIRONMENT
-    sfdx force:mdapi:deploy -l RunLocalTests -d temp_metadata/ -u DevHub -w 10
-    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi    
+    sfdx force:mdapi:deploy -l RunSpecifiedTests -r $TEST_CLASSES -d temp_metadata/ -u DevHub -w 10
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 fi
