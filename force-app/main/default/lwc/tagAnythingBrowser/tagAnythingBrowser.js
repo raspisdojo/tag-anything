0acf61868488140a7835954dfb6f6c2ae43b13ca
Eduardo Asenjo Rodriguez <asenjo.rodriguez.eduardo@gmail.com>
2019-06-28 23:19:01 +0100

    Added change color functaionality by variant picklist

--- force-app/main/default/lwc/tagAnythingBrowser/tagAnythingBrowser.js
+++ force-app/main/default/lwc/tagAnythingBrowser/tagAnythingBrowser.js
@@ -1,14 +1,22 @@
 /* eslint-disable @lwc/lwc/no-async-operation */
-import { LightningElement, track, api, wire } from 'lwc';
-import { getListUi } from 'lightning/uiListApi';
-import { isNullOrWhiteSpace } from 'c/utils';
-import TAG_ANYTHING_OBJECT from '@salesforce/schema/Tag_Anything__c';
+import { LightningElement, track, api, wire } from "lwc";
+import { getListUi } from "lightning/uiListApi";
+import { isNullOrWhiteSpace } from "c/utils";
+import TAG_ANYTHING_OBJECT from "@salesforce/schema/Tag_Anything__c";
 
-const PLACEHOLDER_DEFAULT = 'Search Tag by Label';
-const LISTVIEWAPINAME_DEFAULT = 'All';
+const PLACEHOLDER_DEFAULT = "Search Tag by Label";
+const LISTVIEWAPINAME_DEFAULT = "All";
 const DELAY = 300;
 const PAGESIZE = 2000;
-const FIELDS_REQUIRED = [ 'Name', 'Is_Active__c', 'Label_Color__c', 'Tag_Color__c', 'Icon__c', 'Internal_Value__c' ];
+const FIELDS_REQUIRED = [
+    "Name",
+    "Is_Active__c",
+    "Label_Color__c",
+    "Tag_Color__c",
+    "Icon__c",
+    "Internal_Value__c",
+    "Icon_Variant__c"
+];
 
 export default class TagAnythingBrowser extends LightningElement {
     @api placeholder = PLACEHOLDER_DEFAULT;
@@ -23,29 +31,27 @@ export default class TagAnythingBrowser extends LightningElement {
 
     @wire(getListUi, {
         objectApiName: TAG_ANYTHING_OBJECT,
-        listViewApiName: '$listView',
+        listViewApiName: "$listView",
         pageSize: PAGESIZE,
         fields: FIELDS_REQUIRED
     })
     wiredRecord({ error, data }) {
         if (error) {
-            this.error = 'Unknown error';
+            this.error = "Unknown error";
             if (Array.isArray(error.body)) {
-                this.error = error.body.map((e) => e.message).join(', ');
-            }
-            else if (typeof error.body.message === 'string') {
+                this.error = error.body.map((e) => e.message).join(", ");
+            } else if (typeof error.body.message === "string") {
                 this.error = error.body.message;
             }
             this._tagRecords = undefined;
-        }
-        else if (data) {
+        } else if (data) {
             this._tagRecords = data.records.records;
-            this.prepareMapOfRecords(this._tagRecords, 'Internal_Value__c');
+            this.prepareMapOfRecords(this._tagRecords, "Internal_Value__c");
         }
     }
 
     handleBadgeSelected(evt) {
-        this.dispatchEvent(new CustomEvent('badgeselected', { detail: this._tagRecordsMap[evt.detail] }));
+        this.dispatchEvent(new CustomEvent("badgeselected", { detail: this._tagRecordsMap[evt.detail] }));
     }
 
     handleKeyUp(evt) {
@@ -54,8 +60,7 @@ export default class TagAnythingBrowser extends LightningElement {
         this.delayTimeout = setTimeout(() => {
             if (isNullOrWhiteSpace(query_term)) {
                 this.tagsToDisplay = [];
-            }
-            else {
+            } else {
                 this.tagsToDisplay = this.filterTags(query_term.toLowerCase());
             }
         }, DELAY);
